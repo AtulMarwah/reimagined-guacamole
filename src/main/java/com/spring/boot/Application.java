ffd4786c62a84ff361c765f4952826d5f7dbaf55
Hbk94 <atulmarwah007@gmail.com>
2021-04-02 22:55:40 +0530

    fixx

--- src/main/java/com/spring/boot/Application.java
+++ src/main/java/com/spring/boot/Application.java
@@ -3,19 +3,13 @@ package com.spring.boot;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SuppressWarnings("unused")
 @SpringBootApplication
@@ -35,6 +29,8 @@ public class Application {
             hostname = ip.getHostName();
             System.out.println("Your current IP address : " + ip);
             System.out.println("Your current Hostname : " + hostname);
+            System.out.println("LoopBack Address "+ InetAddress.getLoopbackAddress().getHostAddress());
+            //InetAddress.getLoopbackAddress().getHostName();
             SpringApplication.run(Application.class, args);
  
         } catch (UnknownHostException e) {
